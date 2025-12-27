290030d0873e6a6e02840b03e06a1d97a75ee043
Nico15037 <40710958+Nico15037@users.noreply.github.com>
2025-12-26 23:01:29 -0800

    CLICK HERE TO RUN THE

--- discord-commits/templates/plain.js
+++ discord-commits/templates/plain.js
@@ -3,7 +3,6 @@
  */
 
 export default {
-    message: "Successful commit to **{{ github.context.payload.repository.owner.name }}/{{ github.context.payload.repository.name}}**",
     embed: false,
     extras: [{
         title: "View All Changes",
